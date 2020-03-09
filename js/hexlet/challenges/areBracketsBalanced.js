54f014b4ab864b02266760e66e4329b07be614af
Shtas <Stas@nosoff.biz>
2020-03-09 21:14:37 +0300

    module

--- js/hexlet/challenges/areBracketsBalanced.js
+++ js/hexlet/challenges/areBracketsBalanced.js
@@ -1,7 +1,8 @@
-let areBracketsBalanced = (braces) => {
+const areBracketsBalanced = (braces) => {
     const regexp = new RegExp('\\(\\)|\\[\\]|\\{\\}');
     while ( braces.match(regexp)) {
         braces = braces.replace(regexp, '');
     }
     return braces.length === 0;
 }
+export {areBracketsBalanced};
